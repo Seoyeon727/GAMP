0d6c82307d5b0834a18ed01603c19d5727170c6b
top1572 <wotjd1006@gmail.com>
2024-05-15 10:23:38 +0900

    이거
    어떻게 수정하즤

--- Mario/WorldObject.h
+++ Mario/WorldObject.h
@@ -6,21 +6,21 @@
 
 struct SDL_Surface;
 
-
-class WorldObject 
+// 게임 세계의 모든 오브젝트들의 기본 클래스
+class WorldObject
 {
 protected:
-	Position position;
-	Size size;
-	bool isWithinRangeOfCamera(int beginningOfCamera, int endOfCamera) const;
+    Position position; // 위치
+    Size size; // 크기
+    bool isWithinRangeOfCamera(int beginningOfCamera, int endOfCamera) const; // 카메라 범위 내에 있는지 확인하는 함수
 
 public:
-	int getX() const;
-	int getY() const;
-	int getWidth() const;
-	int getHeight() const;
-	Position getPosition() const;
-	virtual void draw(SDL_Surface* display, int beginningOfCamera, int endOfCamera) const = 0;
+    int getX() const;
+    int getY() const;
+    int getWidth() const;
+    int getHeight() const;
+    Position getPosition() const;
+    virtual void draw(SDL_Surface* display, int beginningOfCamera, int endOfCamera) const = 0; // 세계 안의 오브젝트를 그리는 순수 가상 함수
 };
 
-#endif //WorldObject_H
+#endif // WorldObject_H
